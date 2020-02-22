d5fae5796762cbff7970cf0c19914088e6d50203
ivario123 <49920136+ivario123@users.noreply.github.com>
2020-02-22 15:37:54 +0100

    All the derivatives added

--- MatrixMath/Matrix.java
+++ MatrixMath/Matrix.java
@@ -26,6 +26,38 @@ public class Matrix implements Serializable {
         }
     }
   }
+  //Function derivating anny given matrix
+  static public Matrix derive(Matrix A, int activ){
+    Matrix B = new Matrix(A.m,A.n);
+    switch(activ){
+      case 1:
+      B = dtanh(B);
+      break;
+      case 2:
+      B = dsin(B);
+      break;
+      case 3:
+      B = dcos(B);
+      break;
+      case 4:
+      B = dln(B);
+      break;
+      case 5:
+      B = dgaussian(B);
+      break;
+  }
+  return B;
+  }
+  //Function transposing anny given matrix
+  static public Matrix transpose(Matrix A){
+    Matrix O = new Matrix(A.n,A.m);
+    for(int i = 0; i < O.m; i++){
+      for(int j = 0; j < O.n; j++){
+        O.table[i][j]=A.table[j][i];
+      }
+    }
+    return O;
+  }
   //Function that adds a randum number to each number in the matrix
   static public Matrix rAdd(Matrix m){
     Random r = new Random();
@@ -38,6 +70,23 @@ public class Matrix implements Serializable {
   }
   //Fuction that multiplies to matricies together
   static public Matrix mMult(Matrix A, Matrix B,int activ) {
+    switch(activ){
+      case 1:
+      B = tanh(B);
+      break;
+      case 2:
+      B = sin(B);
+      break;
+      case 3:
+      B = cos(B);
+      break;
+      case 4:
+      B = ln(B);
+      break;
+      case 5:
+      B = gaussian(B);
+      break;
+  }
     Matrix o  = new Matrix(A.m,B.n);
     for(int i = 0; i < A.m; i++){
        for(int j = 0; j < B.n; j++){
@@ -46,23 +95,7 @@ public class Matrix implements Serializable {
         }
        }
     }
-    switch(activ){
-        case 1:
-        o = tanh(o);
-        break;
-        case 2:
-        o = sin(o);
-        break;
-        case 3:
-        o = cos(o);
-        break;
-        case 4:
-        o = ln(o);
-        break;
-        case 5:
-        o = gaussian(o);
-        break;
-    }
+    
     return o;
   }
   //Applies gaussian function to the matrix
